ddb377bdbeb0bcb00be9f5caa4994c053bcaece7
Jeremy Morgan <jeremy@jeremymorgan.com>
2023-03-31 16:55:55 -0700

    fixing images and link

--- erosion_demo.py
+++ erosion_demo.py
@@ -1,12 +1,11 @@
+import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 
 
-image_path = 'images/balloon.png'
-
+image_path = 'images/mustang.png'
 
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
@@ -16,5 +15,6 @@ eroded_image = cv2.erode(image, structuring_element, iterations=1)
 
 plt.figure(figsize=(12, 6))
 plt.subplot(121), plt.imshow(image, cmap='gray'), plt.title('Original Image')
-plt.subplot(122), plt.imshow(eroded_image, cmap='gray'), plt.title('Eroded Image')
+plt.subplot(122), plt.imshow(
+    eroded_image, cmap='gray'), plt.title('Eroded Image')
 plt.show()
--- erosion_demo_elliptical.py
+++ erosion_demo_elliptical.py
@@ -1,20 +1,22 @@
+import matplotlib.pyplot as plt  # Import the Pyplot module from Matplotlib
 import cv2  # Import the OpenCV library
 import numpy as np  # Import the NumPy library
 import matplotlib  # Import the Matplotlib library
 matplotlib.use('TkAgg')  # Set the backend for Matplotlib to 'TkAgg'
-import matplotlib.pyplot as plt  # Import the Pyplot module from Matplotlib
 
 # Define the path to the image
-image_path = 'images/balloon.png'
+image_path = 'images/mustang.png'
 
 # Read the image from the given path, and convert it to grayscale
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
 # Define the structuring element as an ellipse with a 3x3 size
-structuring_element_ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+structuring_element_ellipse = cv2.getStructuringElement(
+    cv2.MORPH_ELLIPSE, (3, 3))
 
 # Perform erosion on the grayscale image using the ellipse-shaped structuring element
-eroded_image_ellipse = cv2.erode(image, structuring_element_ellipse, iterations=1)
+eroded_image_ellipse = cv2.erode(
+    image, structuring_element_ellipse, iterations=1)
 
 # Create a new figure with a specified size
 plt.figure(figsize=(12, 6))
@@ -23,7 +25,8 @@ plt.figure(figsize=(12, 6))
 plt.subplot(121), plt.imshow(image, cmap='gray'), plt.title('Original Image')
 
 # Create the second subplot, showing the eroded image
-plt.subplot(122), plt.imshow(eroded_image_ellipse, cmap='gray'), plt.title('Eroded Image with Ellipse')
+plt.subplot(122), plt.imshow(eroded_image_ellipse,
+                             cmap='gray'), plt.title('Eroded Image with Ellipse')
 
 # Display the subplots
 plt.show()
